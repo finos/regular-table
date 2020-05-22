13b5ff74bb21514b3e28ab6bffaed1c35599089a
Andrew Stein <steinlink@gmail.com>
2020-05-22 15:56:46 -0400

    Merge from `@finos/perspective-viewer-datagrid`

---
+++ packages/perspective-viewer-datagrid/babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                targets: {
+                    chrome: "70",
+                    node: "12",
+                    ios: "13"
+                },
+                modules: process.env.BABEL_MODULE || false,
+                useBuiltIns: "usage",
+                corejs: 3
+            }
+        ]
+    ],
+    sourceType: "unambiguous",
+    plugins: [
+        "lodash",
+        ["@babel/plugin-proposal-decorators", {legacy: true}],
+        "transform-custom-element-classes",
+        "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-proposal-optional-chaining",
+        "./babel-plugin-transform-tagged-literal.js"
+    ]
+};
