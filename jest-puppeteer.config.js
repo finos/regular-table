8994c7f8785b5f1106e938ab115258ce1ef1e7d4
telamonian <telamonian@users.noreply.github.com>
2020-06-07 23:50:01 -0400

    adds a live-reloading dev server to the rollup watch build

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,12 @@
 module.exports = {
     server: {
-        command: "yarn start -p 8081",
+        command: "yarn start --port 8081",
+        launchTimeout: 30000,
         port: 8081,
+        waitOnScheme: {
+            resources: [
+                "dist/umd/regular-table.js",
+              ],
+        },
     },
 };
