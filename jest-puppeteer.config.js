fde8ac4904f9c17f4c7821da6d75ba47f5cf18c7
Andrew Stein <steinlink@gmail.com>
2020-06-11 19:54:18 -0400

    Merge rollup configs, delete `index.html`, remove rollup-plugin-server and reinstate http-server

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,13 +1,10 @@
 module.exports = {
     server: {
-        command: "yarn start --port 8081",
+        command: "yarn start -p 8081",
         launchTimeout: 30000,
         port: 8081,
         waitOnScheme: {
-            resources: [
-                "dist/css/material.css",
-                "dist/umd/regular-table.js",
-              ],
+            resources: ["dist/css/material.css", "dist/umd/regular-table.js"],
         },
     },
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,53 @@
 import babel from "@rollup/plugin-babel";
 import filesize from "rollup-plugin-filesize";
-import postcss from "rollup-plugin-postcss"
+import livereload from "rollup-plugin-livereload";
+import postcss from "rollup-plugin-postcss";
 import sourcemaps from "rollup-plugin-sourcemaps";
 import {terser} from "rollup-plugin-terser";
 
-export default [{
-    input: "src/less/material.less",
-    output: {
-        dir: "dist/css",
-    },
-    plugins: [
-        postcss({
-            inject: false,
-            extract: "material.css",
-            sourceMap: false,
-            minimize: true,
-        }),
-    ],
-    watch: {
-        clearScreen: false,
-    },
-},
-{
-    input: "src/js/index.js",
-    output: {
-        sourcemap: true,
-        file: "dist/umd/regular-table.js",
-    },
-    plugins: [
-        babel({
-            exclude: "node_modules/**",
-            babelHelpers: "bundled",
-        }),
-        filesize(),
-        postcss({
-            inject: false,
-            sourceMap: false,
-            minimize: true,
-        }),
-        sourcemaps(),
-        terser(),
-    ],
-    watch: {
-        clearScreen: false,
-    },
-}];
+export default (args) => {
+    const watch = !!args.watch;
+    return [
+        {
+            input: "src/less/material.less",
+            output: {
+                dir: "dist/css",
+            },
+            plugins: [
+                postcss({
+                    inject: false,
+                    extract: "material.css",
+                    sourceMap: true,
+                    minimize: !watch,
+                }),
+            ],
+            watch: {
+                clearScreen: false,
+            },
+        },
+        {
+            input: "src/js/index.js",
+            output: {
+                sourcemap: true,
+                file: "dist/umd/regular-table.js",
+            },
+            plugins: [
+                babel({
+                    exclude: "node_modules/**",
+                    babelHelpers: "bundled",
+                }),
+                filesize(),
+                watch ? livereload("dist") : terser(),
+                postcss({
+                    inject: false,
+                    sourceMap: watch,
+                    minimize: !watch,
+                }),
+                sourcemaps(),
+            ].filter((x) => x),
+            watch: {
+                clearScreen: false,
+            },
+        },
+    ];
+};
