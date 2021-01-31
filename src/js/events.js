fc7169263aeec94205599e5acd777b2d49442d51
Tim Paine <t.paine154@gmail.com>
2021-01-31 16:44:32 -0500

    map pd->rt

--- src/js/events.js
+++ src/js/events.js
@@ -133,7 +133,7 @@ export class RegularViewEventModel extends RegularVirtualTableViewModel {
                 return;
             }
         }
-        const is_resize = event.target.classList.contains("pd-column-resize");
+        const is_resize = event.target.classList.contains("rt-column-resize");
         const metadata = METADATA_MAP.get(element);
         if (is_resize) {
             await new Promise(setTimeout);
@@ -149,7 +149,7 @@ export class RegularViewEventModel extends RegularVirtualTableViewModel {
                 }
                 td.style.minWidth = "";
                 td.style.maxWidth = "";
-                td.classList.remove("pd-cell-clip");
+                td.classList.remove("rt-cell-clip");
             }
             await this.draw();
         }
@@ -176,7 +176,7 @@ export class RegularViewEventModel extends RegularVirtualTableViewModel {
             }
         }
 
-        const is_resize = event.target.classList.contains("pd-column-resize");
+        const is_resize = event.target.classList.contains("rt-column-resize");
         const metadata = METADATA_MAP.get(element);
         if (is_resize) {
             this._on_resize_column(event, element, metadata);
@@ -236,7 +236,7 @@ export class RegularViewEventModel extends RegularVirtualTableViewModel {
                 const td = row[metadata._virtual_x];
                 if (td) {
                     td.style.maxWidth = td.style.minWidth = override_width + "px";
-                    td.classList.toggle("pd-cell-clip", auto_width > override_width);
+                    td.classList.toggle("rt-cell-clip", auto_width > override_width);
                 }
             }
         }
